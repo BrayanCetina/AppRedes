46d2d8c886b357c113eae61e6f7a9278a92404c8
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2019-11-05 20:25:27 -0600

    quite espacios

--- app/src/main/java/com/example/redes/SesionFragment.java
+++ app/src/main/java/com/example/redes/SesionFragment.java
@@ -2,32 +2,20 @@ package com.example.redes;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import com.example.redes.encrip;
-
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
