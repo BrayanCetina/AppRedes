0690e088dae9558d7838480e41dd8f456ee82d38
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2019-11-07 11:54:59 -0600

    StringRequest haber si jala

--- app/src/main/java/com/example/redes/MainActivity.java
+++ app/src/main/java/com/example/redes/MainActivity.java
@@ -9,6 +9,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import android.widget.TextView;
@@ -49,16 +50,16 @@ public class MainActivity extends AppCompatActivity {
         //String user = eso.MD5(txtUser.getText().toString());
         //String pass = eso.MD5(txtPwd.getText().toString());
         //String url="https://evidential-tubing.000webhostapp.com/usuario_cliente.php?user="+user+"&pwd="+pass;
-        Map<String, String> params = new HashMap();
+        /*Map<String, String> params = new HashMap();
         params.put("user", txtUser.getText().toString());
         params.put("pwd", txtPwd.getText().toString());
         JSONObject obj=new JSONObject(params);
-        /*try {
+        try {
             obj.put("user",txtUser.getText().toString());
             obj.put("pwd",txtPwd.getText().toString());
         } catch (JSONException e) {
             e.printStackTrace();
-        }*/
+        }
 
         JsonObjectRequest jrq = new JsonObjectRequest(Request.Method.POST,url, obj,
                 new Response.Listener<JSONObject>() {
@@ -74,7 +75,34 @@ public class MainActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(MainActivity.this, "no se encontro",Toast.LENGTH_LONG).show();
                     }
-                });
+                });*/
+        StringRequest jrq = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user", txtUser.getText().toString());
+                params.put("pwd", txtPwd.getText().toString());
+                return params;
+            }
+        };
         rq.add(jrq);
     }
 }
