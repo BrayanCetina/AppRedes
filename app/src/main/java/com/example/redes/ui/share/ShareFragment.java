5d58e76d914ee38bc58a3a0aa65f3b8b9358342b
JacobLuna <43625778+JacobLuna@users.noreply.github.com>
2019-11-29 23:09:28 -0600

    haber si jala

--- app/src/main/java/com/example/redes/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/redes/ui/share/ShareFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,9 +16,11 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.redes.R;
 
 public class ShareFragment extends Fragment {
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share, container, false);
+        return inflater.inflate(R.layout.fragment_slideshow, container, false);
     }
 }
--- app/src/main/java/com/example/redes/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/redes/ui/slideshow/SlideshowFragment.java
@@ -1,10 +1,17 @@
 package com.example.redes.ui.slideshow;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -14,10 +21,37 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.redes.R;
 
+import java.io.File;
+
 public class SlideshowFragment extends Fragment {
+
+    WebView webview;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slideshow, container, false);
+        LayoutInflater lf = getActivity().getLayoutInflater();
+        View vista=lf.inflate(R.layout.fragment_tools,container,false);
+        mostrarPDF("prueba.pdf",getContext());
+        return vista;
+
+    }
+    //Procedimiento para mostrar el documento PDF generado
+    public void mostrarPDF(String nombPdf, Context context) {
+        Toast.makeText(context, "Visualizando documento", Toast.LENGTH_LONG).show();
+
+        // Así va correctamente la dirección
+        String dir = Environment.getExternalStorageDirectory()+ "/AppRedes/pdf/" + nombPdf;
+
+        File arch = new File(dir);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(arch), "application/pdf");
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(context, "No existe una aplicación para abrir el PDF", Toast.LENGTH_SHORT).show();
+        }
     }
 }
